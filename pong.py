9da949a3a98c597881fd174297a3480dea547cfe
Maximus True <maximus_true@yahoo.com>
2024-05-17 16:29:50 -0700

    Screen counter, game ends after 5 scores

--- pong.py
+++ pong.py
@@ -7,6 +7,7 @@ def main():
     pygame.init()
 
     DISPLAY=pygame.display.set_mode((1000, 600),0,32)
+    pygame.display.set_caption("Pong")
 
     WHITE = (255,255,255)
     BLUE = (0,0,255)
@@ -15,6 +16,9 @@ def main():
     radius = 12
     velocity = pygame.Vector2()
     velocity.xy = 2.5, 2.5
+    defaultBallVel = pygame.Vector2()
+    defaultBallVel.xy = 2.5, 2.5
+
     acceleration = 0.1
     clock = pygame.time.Clock()
     running = True
@@ -33,7 +37,15 @@ def main():
     player2_pos = pygame.Vector2(DISPLAY.get_width() - DISPLAY.get_width() / 8, DISPLAY.get_height() / 2)
     playerTwoScore = 0
 
+    counter = 0
+    myFont = pygame.font.Font(None, 74)
+
     while True:
+
+        if(playerOneScore == 5):
+            break
+        if(playerTwoScore == 5):
+            break
 
         keys = pygame.key.get_pressed()
 
@@ -52,14 +64,19 @@ def main():
         ball_pos.x += velocity.x
         ball_pos.y += velocity.y
 
+        #Speed up ball
+        if(counter == 750):
+            counter = 0
+            defaultBallVel *= 1.2
+
         if(ball_pos.x + radius > DISPLAY.get_width()):
-            velocity.x = -2.5
+            velocity.x = -defaultBallVel.x
         if(ball_pos.x - radius < 0):
-            velocity.x = 2.5
+            velocity.x = defaultBallVel.x
         if(ball_pos.y + radius > DISPLAY.get_height()):
-            velocity.y = -2.5
+            velocity.y = -defaultBallVel.y
         if(ball_pos.y - radius < 0):
-            velocity.y = 2.5
+            velocity.y = defaultBallVel.y
 
         # Player One
         pygame.draw.rect(DISPLAY, "white", (player_pos.x, player_pos.y, 10, 50))
@@ -93,11 +110,11 @@ def main():
 
         # Collision with player
         if ball_pos.x - radius < player_pos.x + 10 and ball_pos.x - radius > player_pos.x and ball_pos.y + radius > player_pos.y and ball_pos.y - radius < player_pos.y + 50:
-            velocity.x = 2.5
+            velocity.x = defaultBallVel.x
         
         # Collision with player 2
         if ball_pos.x + radius > player2_pos.x and ball_pos.x + radius < player2_pos.x + 10 and ball_pos.y + radius > player2_pos.y and ball_pos.y - radius < player2_pos.y + 50:
-            velocity.x = -2.5
+            velocity.x = -defaultBallVel.x
         
         # Score Reset
         #Player 2 Scores
@@ -105,20 +122,25 @@ def main():
             ball_pos.x = DISPLAY.get_width() / 2
             ball_pos.y = DISPLAY.get_height() / 2
             playerTwoScore += 1
-            print(str(playerOneScore) + " - " + str(playerTwoScore))
+            defaultBallVel.xy = 2.5, 2.5
 
         #Player 1 Scores
         if ball_pos.x + radius > DISPLAY.get_width(): 
             ball_pos.x = DISPLAY.get_width() / 2
             ball_pos.y = DISPLAY.get_height() / 2
             playerOneScore += 1
-            print(str(playerOneScore) + " - " + str(playerTwoScore))
+            defaultBallVel.xy = 2.5, 2.5
+            
+        score_text = myFont.render(f"{playerOneScore}       -       {playerTwoScore}", True, WHITE)
 
-        if keys[K_SPACE]:
-            velocity.xy = -0.25, velocity.y * 0.1
+        DISPLAY.blit(score_text, (DISPLAY.get_width() / 2 - 125, DISPLAY.get_height() / 20))
+
+        counter += 1
         
         # Update the display    
         pygame.display.update()
         pygame.time.delay(10)
+
+    pygame.quit()
 
 main()
