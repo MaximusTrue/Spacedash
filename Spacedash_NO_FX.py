89ace9e72710d861cb63d26333be19680d72578a
Maximus True <maximus_true@yahoo.com>
2024-05-28 19:18:08 -0700

    spacedash no fx

---
+++ Spacedash_NO_FX.py
@@ -0,0 +1,224 @@
+import pygame, sys
+import random
+from pygame.locals import *
+
+def main():
+
+    gameVel = 1.5
+    
+    pygame.init()
+    myFont = pygame.font.Font(None, 200)
+    myFontRest = pygame.font.Font(None, 72)
+    myFontSmall = pygame.font.Font(None, 40)
+
+    pygame.display.set_caption("Spacedash")
+
+    DISPLAY=pygame.display.set_mode((1000,600),0,32)
+
+    WHITE = (255,255,255)
+    BLUE = (0,0,255)
+
+    player = pygame.Vector2()
+    player.xy = 200,200
+    playerVel = 0.6
+    playerAccel = 0.03
+
+    groundHeight = DISPLAY.get_height() - DISPLAY.get_height() / 8
+    
+    MIN_WALL_CLEARANCE = 80
+    wallClearance = 150
+
+    wall = pygame.Vector2()
+
+    wall.xy = DISPLAY.get_width(), random.randint(75+wallClearance, int(groundHeight - 75))
+
+    pointsScored = 0
+    hasScored = False
+
+    collectable = pygame.Vector2()
+    collectable.xy = random.randint(DISPLAY.get_width(), DISPLAY.get_width() + int(DISPLAY.get_width() / 2) ), random.randint(15, int(groundHeight - 15))
+    collectableScore = 0
+
+    dead = False
+
+    invincible = False
+    invincibleTimer = 0
+    invincibleTimeout = 0
+
+    inverse = False
+    blackholeTimer = 0
+
+    #Black Hole
+    blackhole = pygame.Vector2()
+    blackhole.xy = 100000, 0
+    inBlackHole = False
+    blackholePosOffScore = random.randint(7, 7) + pointsScored
+
+    while True:
+        DISPLAY.fill(WHITE)
+
+        for event in pygame.event.get():
+            if event.type==QUIT:
+                pygame.quit()
+                sys.exit()
+            #Test Inverse
+            if event.type == pygame.KEYDOWN and event.key==K_UP:
+                inverse = True
+            #Invincible
+            if event.type == pygame.KEYDOWN and event.key==K_RETURN and collectableScore - 5 >= 0:
+                invincible = True
+                collectableScore -= 5
+                invincibleTimeout += 500
+            #Slow Down
+            if event.type == pygame.KEYDOWN and event.key == K_RSHIFT and collectableScore - 5>= 0:
+                gameVel = gameVel / 2
+                collectableScore -= 5
+
+        #Black Hole
+        if checkCollision(player.x, player.y, 50, 50, blackhole.x + 50, blackhole.y, 50, DISPLAY.get_height()) and not inBlackHole:
+            inBlackHole = True
+            inverse = not inverse
+            if inverse:
+                print("INVERSED")
+            blackholePosOffScore = random.randint(7, 10) + pointsScored
+            print("IN BLACK HOLE")
+
+        if inBlackHole:
+            blackholeTimer += 1
+
+        #Stop Inverse Time
+        if blackholeTimer == 200:
+            blackholeTimer = 0
+            inBlackHole = False
+            print("BLACKHOLE OVER")
+
+        #Reset Wall
+        if wall.x < -50:
+            wall.y = random.randint(75+wallClearance, int(groundHeight - 75)) # - wallClearance
+            wall.x = DISPLAY.get_width()
+            hasScored = False
+            if blackholePosOffScore - 1 == pointsScored:
+                blackhole.x = wall.x
+
+        pygame.draw.rect(DISPLAY, "purple", (blackhole.x, blackhole.y, 50, DISPLAY.get_height()))
+        #Create random obstical
+        pygame.draw.rect(DISPLAY, "red", (wall.x, wall.y, 50, DISPLAY.get_height())) # Bottom
+        pygame.draw.rect(DISPLAY, "red", (wall.x, 0, 50, wall.y - wallClearance)) # Top
+        
+        
+        blackhole.x = blackhole.x - gameVel
+
+        wall.x = wall.x - gameVel
+
+        #Player Gravity
+        pygame.draw.rect(DISPLAY, BLUE, (player.x, player.y, 50, 50))
+        player.y = player.y + playerVel
+
+        if not inverse:
+            playerVel += playerAccel
+        else:
+            playerVel -= playerAccel
+
+        #Ground
+        pygame.draw.rect(DISPLAY, "black", (0, groundHeight, DISPLAY.get_width(), 10))
+
+        #Slide on ground
+        if player.y + 50 > groundHeight:
+            player.y = groundHeight - 50
+        
+        #Slide on Ceiling
+        if player.y < 0:
+            player.y = 0
+
+        keys = pygame.key.get_pressed()
+
+        #Flying
+        if keys[K_SPACE]and not dead and not inverse:
+            playerVel = 0.6
+            player.y -= 3.5 #4
+        elif keys[K_SPACE] and not dead:
+            playerVel = -0.6
+            player.y += 3.5
+
+        #Die
+        if checkWallCollision(player.x, player.y, wall.x, wall.y, wallClearance) and not invincible:
+            gameVel = 0
+            playerVel = 0
+            dead = True
+            resetText = myFontRest.render("Press P to Reset", True, "black")
+            DISPLAY.blit(resetText, (DISPLAY.get_width() / 2 - 150, DISPLAY.get_height() / 2))
+        
+        #Reset
+        if dead and keys[K_p]:
+            gameVel = 1.5
+            playerVel = 0.6
+            player.xy = 200,200
+            wall.xy = DISPLAY.get_width(), random.randint(75+wallClearance, int(groundHeight - 75))
+            pointsScored = 0
+            hasScored = False
+            dead = False
+            collectable.xy = random.randint(DISPLAY.get_width(), DISPLAY.get_width() + int(DISPLAY.get_width() / 2) ), random.randint(15, int(groundHeight - 15))
+            collectableScore = 0
+            inverse = False
+            blackhole.xy = 1000000, 0
+            inBlackHole = False
+            blackholePosOffScore = random.randint(7, 10) + pointsScored
+            blackholeTimer = 0
+
+        #Score Points
+        if player.x > wall.x + 50 and not hasScored:
+            pointsScored += 1
+            hasScored = True
+            gameVel += 1.5*0.1
+
+        #Collectable
+        pygame.draw.rect(DISPLAY, "green", (collectable.x, collectable.y, 15, 15))
+        collectable.x -= gameVel
+
+        #Respawn Collectable if not Collected
+        if(collectable.x < 0):
+            collectable.xy = random.randint(DISPLAY.get_width(), DISPLAY.get_width() + int(DISPLAY.get_width() / 2) ), random.randint(15, int(groundHeight - 15))
+        
+        #Collecting Collectable
+        if checkCollision(player.x, player.y, 50, 50, collectable.x, collectable.y, 15, 15):
+            collectableScore = collectableScore + 1
+            collectable.x = random.randint(DISPLAY.get_width(), DISPLAY.get_width() + int(DISPLAY.get_width() / 2))
+            collectable.y = random.randint(15, int(groundHeight - 15))
+        
+        #Making Sure Collectable isn't in a Wall
+        if checkWallCollision(collectable.x, collectable.y, wall.x, wall.y, wallClearance):
+            collectable.x = collectable.x - 100
+        
+        #Score Display
+        scoreText = myFont.render(str(pointsScored), True, "grey")
+        DISPLAY.blit(scoreText, (DISPLAY.get_width() / 2 - 50, 0))
+
+        #Collectable Counter Display
+        collectableText = myFontSmall.render("Counter: " + str(collectableScore), True, "black")
+        DISPLAY.blit(collectableText, (0,0))
+
+        if invincible:
+            invincibleTimer += 1
+            print(invincibleTimer)
+
+        if invincibleTimer == invincibleTimeout:
+            invincible = False
+            invincibleTimer = 0
+            invincibleTimeout = 0
+
+        if keys[K_a]:
+            gameVel = gameVel - 0.01
+        if keys[K_d]:
+            gameVel = gameVel + 0.01
+
+        pygame.draw.rect(DISPLAY, "white", (0, groundHeight + 10, DISPLAY.get_width(), DISPLAY.get_height() - groundHeight))
+        pygame.display.update()
+        pygame.time.delay(10)
+
+def checkWallCollision(x1, y1, x2, y2, wallClearance):
+    return (x2 <= x1 + 50 and x1 <= x2 + 50) and (y1 + 50 >= y2 or y1 <= y2 - wallClearance)
+
+def checkCollision(x1, y1, width_1, height_1, x2, y2, width_2, height_2):
+    return x1 + width_1 >= x2 and x1 <= x2 + width_2 and y1 + width_1 >= y2 and y1 <= y2 + height_2
+    
+main()
